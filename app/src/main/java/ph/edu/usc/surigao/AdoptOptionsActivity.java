e728176d94f760f23fb32baaa5a13f6428c1a122
0murice <daphnesurigao@gmail.com>
2025-02-17 09:58:08 -0900

    added filter function

--- app/src/main/java/ph/edu/usc/surigao/AdoptOptionsActivity.java
+++ app/src/main/java/ph/edu/usc/surigao/AdoptOptionsActivity.java
@@ -8,22 +8,24 @@ import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
 import android.widget.Spinner;
-import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class AdoptOptionsActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener{
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
-    Button btnLogout, btnCheckCart, btnAddtoCart;
+public class AdoptOptionsActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
 
+    Button btnLogout, btnCheckCart;
     Spinner spinProduct;
     ListView list;
 
-    private String[] products = { "Pusheen Cookie", "Pusheen Toes", "Pusheen Keychain", "Giant Pusheen", "Pusheen Pouch", "Pusheen Hooodie",};
-
-    private String[] prices = {"$100", "$150", "30", "$9999", "$15", "$75"};
+    private String[] products = {"Display All", "Pusheen Cookie", "Pusheen Toes", "Pusheen Keychain", "Giant Pusheen", "Pusheen Pouch", "Pusheen Hoodie"};
+    private String[] prices = {"$0", "$100", "$150", "$30", "$9999", "$15", "$75"};
 
     Integer[] imgid = {
+            R.drawable.default_image,
             R.drawable.option1,
             R.drawable.option2,
             R.drawable.option3,
@@ -31,6 +33,11 @@ public class AdoptOptionsActivity extends AppCompatActivity implements AdapterVi
             R.drawable.option5,
             R.drawable.option6
     };
+
+    private List<String> filteredProducts;
+    private List<String> filteredPrices;
+    private List<Integer> filteredImages;
+    private MyProductAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,21 +52,15 @@ public class AdoptOptionsActivity extends AppCompatActivity implements AdapterVi
 
         list = findViewById(R.id.list);
 
-        ArrayAdapter<String> arrProducts = new ArrayAdapter<>(
-                this, android.R.layout.simple_spinner_item,
-                products
-        );
-
+        ArrayAdapter<String> arrProducts = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, products);
+        arrProducts.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinProduct.setAdapter(arrProducts);
 
-        ArrayAdapter<String> arrPrices = new ArrayAdapter<>(
-                this, android.R.layout.simple_spinner_dropdown_item,
-                prices
-        );
+        filteredProducts = new ArrayList<>(Arrays.asList(products).subList(1, products.length));
+        filteredPrices = new ArrayList<>(Arrays.asList(prices).subList(1, prices.length));
+        filteredImages = new ArrayList<>(Arrays.asList(imgid).subList(1, imgid.length));
 
-        list.setAdapter(arrPrices);
-
-        MyProductAdapter adapter = new MyProductAdapter( this, products, prices, imgid);
+        adapter = new MyProductAdapter(this, filteredProducts, filteredPrices, filteredImages);
         list.setAdapter(adapter);
 
         setupUI();
@@ -67,31 +68,40 @@ public class AdoptOptionsActivity extends AppCompatActivity implements AdapterVi
 
     @Override
     public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-        Toast.makeText(getApplicationContext(), products[position], Toast.LENGTH_SHORT).show();
+        filterProducts(position);
     }
 
     @Override
-    public void onNothingSelected(AdapterView<?> parent) {
+    public void onNothingSelected(AdapterView<?> parent) {}
 
+    private void filterProducts(int position) {
+        filteredProducts.clear();
+        filteredPrices.clear();
+        filteredImages.clear();
+
+        if (position == 0) {
+            filteredProducts.addAll(Arrays.asList(products).subList(1, products.length));
+            filteredPrices.addAll(Arrays.asList(prices).subList(1, prices.length));
+            filteredImages.addAll(Arrays.asList(imgid).subList(1, imgid.length));
+        } else {
+            filteredProducts.add(products[position]);
+            filteredPrices.add(prices[position]);
+            filteredImages.add(imgid[position]);
+        }
+
+        adapter.notifyDataSetChanged();
     }
 
     public void setupUI() {
-        btnLogout.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent optionsIntent = new Intent(AdoptOptionsActivity.this, AdoptLoginActivity.class);
-                startActivity(optionsIntent);
-                finish();
-            }
+        btnLogout.setOnClickListener(v -> {
+            Intent optionsIntent = new Intent(AdoptOptionsActivity.this, AdoptLoginActivity.class);
+            startActivity(optionsIntent);
+            finish();
         });
 
-        btnCheckCart.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent optionsIntent = new Intent(AdoptOptionsActivity.this, AdoptCheckoutActivity.class);
-                startActivity(optionsIntent);
-            }
+        btnCheckCart.setOnClickListener(v -> {
+            Intent optionsIntent = new Intent(AdoptOptionsActivity.this, AdoptCheckoutActivity.class);
+            startActivity(optionsIntent);
         });
-
     }
 }
--- app/src/main/java/ph/edu/usc/surigao/MyProductAdapter.java
+++ app/src/main/java/ph/edu/usc/surigao/MyProductAdapter.java
@@ -10,14 +10,16 @@ import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import java.util.List;
+
 public class MyProductAdapter extends ArrayAdapter<String> {
 
     private final Activity context;
-    private final String[] products;
-    private final String[] prices;
-    private final Integer[] imgid;
+    private final List<String> products;
+    private final List<String> prices;
+    private final List<Integer> imgid;
 
-    public MyProductAdapter(Activity context, String[] products, String[] prices, Integer[] imgid) {
+    public MyProductAdapter(Activity context, List<String> products, List<String> prices, List<Integer> imgid) {
         super(context, R.layout.product_item, products);
         this.context = context;
         this.products = products;
@@ -27,7 +29,6 @@ public class MyProductAdapter extends ArrayAdapter<String> {
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
-
         LayoutInflater inflater = context.getLayoutInflater();
         View rowView = inflater.inflate(R.layout.product_item, null, true);
 
@@ -36,15 +37,15 @@ public class MyProductAdapter extends ArrayAdapter<String> {
         TextView textsub = rowView.findViewById(R.id.productprice);
         Button addButton = rowView.findViewById(R.id.addtocart);
 
-        img.setImageResource(imgid[position]);
-        textheader.setText(products[position]);
-        textsub.setText(prices[position]);
+        img.setImageResource(imgid.get(position));
+        textheader.setText(products.get(position));
+        textsub.setText(prices.get(position));
 
         addButton.setOnClickListener(v -> {
             Intent detailIntent = new Intent(context, AdoptOptionDetailActivity.class);
-            detailIntent.putExtra("productName", products[position]);
-            detailIntent.putExtra("productPrice", prices[position]);
-            detailIntent.putExtra("productImage", imgid[position]);
+            detailIntent.putExtra("productName", products.get(position));
+            detailIntent.putExtra("productPrice", prices.get(position));
+            detailIntent.putExtra("productImage", imgid.get(position));
             context.startActivity(detailIntent);
         });
 
